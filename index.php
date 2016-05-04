40862380259307bfdedc4aefb1b81665c8e6cb5d
mbusson <mbusson@users.noreply.github.com>
2016-05-04 11:18:00 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/ >
+	 <link rel="stylesheet" href="styles.css">
+	 <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	 <link href='http://fonts.googleapis.com/css?family=Signika+Negative:300,400,700' rel='stylesheet' type='text/css'/>
+</head>
+<body>
+
+	<div id="loader-wrapper">
+	    <div id="loader"></div>
+	    <div class="loader-section section-left"></div>
+	    <div class="loader-section section-right"></div>
+	</div>
+
+	
+
+
+	<div id="animated">  
+		<div class="product" id="welcome">Welcome</div><br>
+		<div class='tagline'>T</div>
+		<div class='tagline'>h</div>
+		<div class='tagline'>i</div>
+		<div class='tagline'>s</div>
+		<div class='tagline'> </div>
+		<div class='tagline'>i</div>
+		<div class='tagline'>s</div>
+		<div class='tagline'> </div>
+		<div class='tagline'>a</div>
+		<div class='tagline'>n</div>
+		<div class='tagline'> </div>
+		<div class='tagline'>a</div>
+		<div class='tagline'>n</div>
+		<div class='tagline'>i</div>
+		<div class='tagline'>m</div>
+		<div class='tagline'>a</div>
+		<div class='tagline'>t</div>
+		<div class='tagline'>e</div>
+		<div class='tagline'>d</div>
+		<div class='tagline'></div>
+		<div class='tagline'>t</div>
+		<div class='tagline'>e</div>
+		<div class='tagline'>x</div>
+		<div class='tagline'>t</div>
+		<div class='tagline'>.</div><br>
+		<div id="enter">Enter</div>
+	</div>
+
+
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.4/TweenMax.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="style.js"></script>
+</body>
+</html>
